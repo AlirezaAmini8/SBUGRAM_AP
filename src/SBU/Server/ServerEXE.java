4e1bb2c2fb5dcb274cc42d57215a44fb38fe6ac2
Alireza <alirezaamini.138115@gmail.com>
2021-06-05 19:21:58 +0430

    some classes have been updated

--- src/SBU/Server/ServerEXE.java
+++ src/SBU/Server/ServerEXE.java
@@ -23,22 +23,6 @@ public class ServerEXE {
         return isServerUp;
     }
 
-    /*public static void checkValidMail(Mail mail){
-        String recieverName = mail.getReciever();
-        if (recieverName.equals("mailerdaemon")) return; // loop!
-        Profile recieverProf = profiles.get(recieverName);
-        if (recieverProf != null) return; //its ok
-
-        String message = "Dear user\nwe are afraid that tell you we can't handle your recent request\nthe address you requested is not valid in our servers\n";
-        Mail answer = new Mail("mailerdaemon", mail.getSender() , "Mail Delivery Subsystem" , message);
-
-        Map<String,Object> outcome =  new HashMap<>();
-        outcome.put("mail",answer);
-        outcome.put("command",Command.SEND_MAIL);
-        API.sendMail(outcome);
-        ServerEXE.mails.remove(mail); // delete invalid mail from server!
-    }*/
-
     public static void main(String[] args) {
         DBManager.getInstance().initializeServer();
 
